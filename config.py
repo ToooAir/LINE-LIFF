790eb1c00def9a9133b4e7e28ae38c01c9d8b4e5
ToooAir <33440687+ToooAir@users.noreply.github.com>
2021-06-06 18:53:55 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 config = {
-    'LINE_CHANNEL_SECRET': '31e924342ffa2bce12c004b1d6a1d611',
-    'LINE_CHANNEL_ACCESS_TOKEN': 'OsZl/cGwEraPJmbK3TuJfxioXJ0vZ4v7XeQBSUVt1qZzyVA7GqUftQhZR6sxO7XJbfHvm2vS4X02EXSoFHmdkMX36dLn8y9EqPlh8UrA3G+2LU2z846zvwpmWqqV8udc8P8e/pPCBPhO1AzY7qu/iAdB04t89/1O/w1cDnyilFU=',
+    'LINE_CHANNEL_SECRET': '',
+    'LINE_CHANNEL_ACCESS_TOKEN': '',
     'mysql': {
         'dialect': 'mysql',
         'driver': 'pymysql',
